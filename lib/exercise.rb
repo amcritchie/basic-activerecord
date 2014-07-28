f041f57da3b0f06a732fbfa413982d46323f85f6
Alex McRitchie <amcritchie@gmail.com>
2014-07-28 12:03:43 -0600

    3rd commit lunch time

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -4,6 +4,8 @@ require "./lib/item"
 require "./lib/orderitem"
 require "./lib/order"
 
+p "="*100
+
 # p Customer.all
 
 # p Customer.first
@@ -16,4 +18,28 @@ require "./lib/order"
 
 # p Customer.limit(5)
 
-p Customer.order(name: :asc)
+# p Customer.order(:name :asc)
+
+# p Customer.order(:name :desc)
+
+# p Item.all
+
+# p Item.where('name LIKE ?', 'boot%')
+
+# p Order.all
+
+# p Order.limit(5)
+
+# p Order.order(:id :desc).limit(5)
+
+# p Customer.select(:id, :name, :email)
+
+# p Customer.select(:id, :name, :address)
+
+# p Customer.count
+
+# p Order.sum(:amount).to_f
+
+# p Order.where(customer_id: 1).sum(:amount).to_f
+
+p Order.average(:amount).to_f
